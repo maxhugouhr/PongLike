24771bec93585cca0bc1ab1aec2ee53f66050b2a
maxhugouhr <uhr.max@gmail.com>
2021-02-22 08:10:20 -0700

    completed the hitbox checking and reflection/deflection functions. Next step is to create the player with keyboard inputs

--- surface.py
+++ surface.py
@@ -1,6 +1,7 @@
 from reflector import Reflector
 import math
 import pygame as pg
+import time
 
 class Surface(Reflector):
     #For vertical surfaces, the left endpoint is defined as the top point and the right endpoint is the bottom
@@ -24,10 +25,22 @@ class Surface(Reflector):
         self.isReflector = reflector #if surface is a reflector then it reflects the ball, otherwise it deflects the ball
         self.speedMultiplier = speedMultiplier #multiplies the ball speed when hit
         self.deflectionAngle = defAngle
+        self.lastHitTime = time.time_ns()
 
 
     def draw(self, img):
         pg.draw.line(img, self.color, self.leftEndpoint, self.rightEndpoint, self.width)
+
+
+    def impact(self,ball):
+        if abs(self.lastHitTime - time.time_ns()) > 500000000:
+            if self.isReflector:
+                self.reflect(ball)
+                self.lastHitTime = time.time_ns()
+            else:
+                self.deflect(ball)
+                self.lastHitTime = time.time_ns()
+
 
 
     def reflect(self,ball):
@@ -39,13 +52,16 @@ class Surface(Reflector):
         refTransBallVeloc = (math.cos(flatBallAngle), -math.sin(flatBallAngle))
         transOutAngle = math.atan2(refTransBallVeloc[1],refTransBallVeloc[0])
         actualOutAngle = transOutAngle + self.angleToHor
-        ball.velocity[0] = ballMagVeloc*math.cos(actualOutAngle)
-        ball.velocity[1] = ballMagVeloc*math.sin(actualOutAngle)
+        ball.velocity[0] = ballMagVeloc*math.cos(actualOutAngle)*self.speedMultiplier
+        ball.velocity[1] = ballMagVeloc*math.sin(actualOutAngle)*self.speedMultiplier
 
 
     def deflect(self,ball):
-        # TODO implement this method
-        pass
+        ballAngle = float(math.atan2(ball.velocity[1], ball.velocity[0]))  # angle with respect to the x axis
+        outAngle = ballAngle + self.deflectionAngle
+        ballMagVeloc = math.sqrt(ball.velocity[1] ** 2 + ball.velocity[0] ** 2)
+        ball.velocity[0] = ballMagVeloc * math.cos(outAngle) * self.speedMultiplier
+        ball.velocity[1] = ballMagVeloc * math.sin(outAngle) * self.speedMultiplier
 
 
     def checkHit(self,ball):
@@ -53,12 +69,15 @@ class Surface(Reflector):
         if (self.leftEndpoint[0] == self.rightEndpoint[0]): #if the surface is vertical
             if (ball.position[1] < self.rightEndpoint[1] and ball.position[1] > self.leftEndpoint[1]):
                 if abs(self.leftEndpoint[0] - ball.position[0]) < ballVelocityMag:
-                    self.reflect(ball)
+                    self.impact(ball)
         elif (self.leftEndpoint[1] == self.rightEndpoint[1]): #if the surface is horizontal
             if (ball.position[0] < self.rightEndpoint[0] and ball.position[0] > self.leftEndpoint[0]):
                 if abs(ball.position[1] - self.leftEndpoint[1]) < ballVelocityMag:
-                    self.reflect(ball)
+                    self.impact(ball)
         else:
             dy = self.rightEndpoint[1] - self.leftEndpoint[1]
             dx = self.rightEndpoint[0] - self.leftEndpoint[0]
             y = lambda a: (dy/dx)*(a - self.leftEndpoint[0]) + self.leftEndpoint[1] #gives the y value of the surface for a given x value
+            surfYValue = y(ball.position[0])
+            if abs(surfYValue - ball.position[1]) < ballVelocityMag:
+                self.impact(ball)
